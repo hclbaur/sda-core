32d597df5191a198f04764960d8856c63c9ad826
hclbaur <hclbaur@gmail.com>
2022-08-11 13:41:38 +0200

    Improved demo.

--- src/test/java/demo.java
+++ src/test/java/demo.java
@@ -12,8 +12,10 @@ public class demo {
 		
 		FileReader fin = new FileReader(args[0]);
 		Node root = (new SDAParser()).parse(fin);
-
-		for (Node contact : root.getNodes().get("contact")) {
+		
+		if (! root.hasNodes()) return;
+		
+		for (Node contact : root.getNodes().get("contact").get(n -> n.hasNodes())) {
 			
 			Node name = contact.getNodes().get("firstname").get(1);
 			NodeSet numbers = contact.getNodes().get("phonenumber");
@@ -24,26 +26,5 @@ public class demo {
 				System.out.println("  Number " + ++i + ": " + number.getValue());
 			}
 		}
-		
-		
-//		A (somewhat) more defensive version	
-//	
-//		if (! root.hasNodes()) return;
-//		NodeSet contacts = root.getNodes().get("contact").get(n -> n.hasNodes());
-//		
-//		for (Node contact : contacts) {
-//			
-//			Node name = contact.getNodes().get("firstname").get(n -> !n.hasNodes()).get(1);
-//			NodeSet numbers = contact.getNodes().get("phonenumber").get(n -> !n.hasNodes());
-//			
-//			System.out.println((name == null ? "An unnamed contact" : name.getValue()) 
-//					+ " has " + numbers.size() + " phone number(s).");
-//			
-//			int i = 0; for (Node number : numbers) {
-//				System.out.println("  Number " + ++i + ": " + number.getValue());
-//			}	
-//		}
-
 	}
-
 }
